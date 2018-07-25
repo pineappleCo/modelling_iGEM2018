7003eeaed07e494166d4904fe7d819bcf270b23b
pineappleCo <freddie.starkey@gmail.com>
2018-07-25 16:15:07 +0100

    per sec -> per min

--- rates.py
+++ rates.py
@@ -10,28 +10,28 @@ anderson_str = [1., 0.7, 0.86, 0.01, 0.72, 0.24, 0.47, 0.36, 0.51, 0.04, 0.33, 0
 rbs_affinity = [0.6, 0.3, 0.07]
 
 #speed of RNA polymerase (60nt/s) / length (nt), imm not expressed in maxicells
-imm_transcription_rate = 60./258.
-col_transcription_rate = 60./1743.
+imm_transcription_rate = (60.*60.)/258.
+col_transcription_rate = (60.*60.)/1743.
 imm_transcription_rate_maxicell = 0.
 
 # average mRNA half-life in e. coli - 5min - http://book.bionumbers.org/
 # exponential decay constant = ln(2)/half-life
-mRNA_deg_rate = log(2)/300.
+mRNA_deg_rate = log(2)/5.
 
 # movement speed of ribosome (20 nt/s - http://book.bionumbers.org/) / num
 # amino acids  
-imm_translation_rate = 20./83.
-col_translation_rate = 20./581.
+imm_translation_rate = (20.*60.)/83.
+col_translation_rate = (20.*60)/581.
 
 # effective halflife of proteins with halflife greater than generation length 
-# in micro-organisms is the generation length - 40min in e coli
+# in micro-organisms is the generation length - 20min in e coli
 # exponential decay constant = ln(2)/half-life
-imm_deg_rate = log(2)/((40./60.)*3600.)
-col_deg_rate = log(2)/((40./60.)*3600.)
+imm_deg_rate = log(2)/20.
+col_deg_rate = log(2)/20.
 
 # expasy protparam - https://web.expasy.org/protparam/ - estimates half-life  
 # >10hrs in E. coli and 30hrs in reticulocyte, average at 20hrs
 # exponential decay constant = ln(2)/half-life
-imm_deg_rate_maxicell = log(2)/(20.*3600.)
-col_deg_rate_maxicell = log(2)/(20.*3600.)
+imm_deg_rate_maxicell = log(2)/(20.*60)
+col_deg_rate_maxicell = log(2)/(20.*60)
 
